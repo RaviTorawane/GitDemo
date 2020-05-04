7accb38a8634f7fd6677f12cd7186e8abf9a2fbc
ravi <ravitorawane@gmail.com>
2020-05-04 23:57:55 +0530

    FIRST COMMIT

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class AppiumTest {
+	@Test
+	public void Android()
+	{
+		System.out.println("Android");
+	}
+	@Test
+	public void IOS()
+	{
+		System.out.println("IOS");
+	}
+}
---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
